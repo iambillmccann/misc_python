bd173aff48ec7ed7be9dbc0c5dbf041f6a2e4ea9
Bill McCann <bill.mccann@gmail.com>
2024-11-22 10:35:47 -0500

    Update vault_bank_account.py

--- paypal poc/vault_bank_account.py
+++ paypal poc/vault_bank_account.py
@@ -1,15 +1,43 @@
 import requests
-from utilities import get_secret
+import json
+import boto3
 
-PAYPAL_CLIENT_ID = get_secret()["vbrato/paypal/clientid"]
-PAYPAL_SECRET = get_secret()["vbrato/paypal/secret"]
-PAYPAL_API_BASE = (
-    "https://api-m.sandbox.paypal.com"  # ToDo: Move to environment variable
-)
+# from utilities import get_secret
+
+
+def get_secret():
+    """
+    Get a secret from AWS Secrets Manager using the secret ARN.
+
+    Docs: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/secretsmanager/client/get_secret_value.html
+
+    Args:
+        secret_arn: The ARN of the secret to get
+    Returns:
+        The secret string, usually a dictionary (e.g. {"vbrato/stripe/secret": "sk_test.....", "vbrato/stripe/key": "pk_test.....",})
+    """
+
+    session = boto3.Session()
+    client = session.client(service_name="secretsmanager", region_name="us-east-1")
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId="PayPalSecret")
+    except Exception as e:
+        raise e
+
+    secret = json.loads(get_secret_value_response["SecretString"])
+    return secret
 
 
 def get_paypal_access_token():
     """Retrieve PayPal OAuth access token"""
+
+    PAYPAL_CLIENT_ID = get_secret()["vbrato/paypal/clientid"]
+    PAYPAL_SECRET = get_secret()["vbrato/paypal/secret"]
+    PAYPAL_API_BASE = (
+        "https://api-m.sandbox.paypal.com"  # ToDo: Move to environment variable
+    )
+
     response = requests.post(
         f"{PAYPAL_API_BASE}/v1/oauth2/token",
         headers={"Accept": "application/json", "Accept-Language": "en_US"},
