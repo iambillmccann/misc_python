d6a66730f28b4efc12ab046615f2619c0c2dc2b9
Bill McCann <bill.mccann@gmail.com>
2025-01-02 10:15:01 -0500

    Show all 265 repositories

--- github_cleanup/main.py
+++ github_cleanup/main.py
@@ -9,10 +9,29 @@ token = os.getenv("GITHUB_TOKEN")
 org = os.getenv("GITHUB_ORG")
 
 
-def main():
+def get_all_repos(org, token):
     headers = {"Authorization": f"token {token}"}
-    response = requests.get(f"https://api.github.com/orgs/{org}/repos", headers=headers)
-    repos = response.json()
+    repos = []
+    page = 1
+
+    while True:
+        response = requests.get(
+            f"https://api.github.com/orgs/{org}/repos",
+            headers=headers,
+            params={"page": page, "per_page": 100},
+        )
+        if response.status_code != 200:
+            break
+        repos.extend(response.json())
+        if len(response.json()) == 0:
+            break
+        page += 1
+
+    return repos
+
+
+def main():
+    repos = get_all_repos(org, token)
     for repo in repos:
         print(repo["name"])
 
