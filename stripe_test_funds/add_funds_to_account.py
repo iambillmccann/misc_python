17937f667eb923e08ef8ca79e7dc9153be63f12a
Bill McCann <bill.mccann@gmail.com>
2025-02-04 10:13:56 -0500

    Switch to pm_card_visa

--- stripe_test_funds/add_funds_to_account.py
+++ stripe_test_funds/add_funds_to_account.py
@@ -6,7 +6,7 @@ stripe.api_key = env.STRIPE_SECRET_KEY
 
 try:
     # Create a token using the test card number that deposits funds immediately.
-    token_response = stripe.Token.create(
+    _token_response = stripe.Token.create(
         card={
             "number": "4000000000000077",  # Test card for available balance
             "exp_month": 12,
@@ -15,14 +15,14 @@ try:
         }
     )
 
-    print(f"Token created: {token_response.id}")
+    # print(f"Token created: {token_response.id}")
 
     # Create a test charge to add funds to your account
     charge = stripe.Charge.create(
         amount=500000,  # amount in cents, e.g., $500.00
         currency="usd",
-        # source="tok_visa",  # Use Stripe's test token
-        source=token_response.id,  # Use the token ID from the previous step
+        source="pm_card_visa",  # Use Stripe's test token
+        # source=token_response.id,  # Use the token ID from the previous step
         description="Test charge to add funds to company account",
     )
 except Exception as e:
